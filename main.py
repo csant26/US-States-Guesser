af8ce70200b092e38086dae37824de1c4079072e
csant26 <shishantstha@gmail.com>
2026-02-03 12:47:48 +0545

    feat: added the initial image layout for the game.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+"""US States guesser"""
 
+import turtle
+import constants as cons
+
+class USStatesGuesser:
+    """US States Guesser class"""
+    def __init__(self):
+        self.initialize_screen()
+        self.initialize_turtle()
+
+    def initialize_screen(self):
+        """Initialize screen"""
+        self.screen = turtle.Screen()
+        self.screen.title("US States Game")
+        self.screen.addshape(cons.IMAGE_PATH)
+
+    def initialize_turtle(self):
+        """Initialize turtle"""
+        self.my_turtle = turtle.Turtle()
+        self.my_turtle.shape(cons.IMAGE_PATH)
+
+    def run(self):
+        """Runs the game"""
+        self.screen.mainloop()
+
+game = USStatesGuesser()
+game.run()
